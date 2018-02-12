7c416406f1ff5064b06296733ad62d45571cf6c3
Alex Welk <alexwelk.92@gmail.com>
2018-02-12 18:27:45 -0500

    Initial commit

---
+++ src/test/java/com/isz/resume/MustBeResumeTimeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.isz.resume;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MustBeResumeTimeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
