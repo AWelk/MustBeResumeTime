c99cbaaa4731daa58533e4590e887240ce98ac73
Alex Welk <alexwelk.92@gmail.com>
2018-03-03 00:11:16 -0500

    Fixed testing error

--- src/main/frontend/src/app/resume/misc/misc.component.spec.ts
+++ src/main/frontend/src/app/resume/misc/misc.component.spec.ts
@@ -1,20 +1,20 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import {SkillsComponent} from './misc.component';
+import {MiscComponent} from './misc.component';
 
 describe('SkillsComponent', () => {
-  let component: SkillsComponent;
-  let fixture: ComponentFixture<SkillsComponent>;
+  let component: MiscComponent;
+  let fixture: ComponentFixture<MiscComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [SkillsComponent]
+      declarations: [MiscComponent]
     })
       .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SkillsComponent);
+    fixture = TestBed.createComponent(MiscComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
