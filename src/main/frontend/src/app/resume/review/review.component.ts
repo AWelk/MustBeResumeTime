252c217cdb2dc4256979f8040ff45852b3276dc0
Alex Welk <alexwelk.92@gmail.com>
2018-03-02 23:58:05 -0500

    Added review component

--- src/main/frontend/src/app/resume/review/review.component.ts
+++ src/main/frontend/src/app/resume/review/review.component.ts
@@ -1,16 +1,26 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, TemplateRef} from '@angular/core';
+import {FormTab} from "../resume.component";
+import {BsModalRef, BsModalService} from "ngx-bootstrap";
 
 @Component({
   selector: 'app-review',
   templateUrl: './review.component.html',
   styleUrls: ['./review.component.css']
 })
-export class ReviewComponent implements OnInit {
+export class ReviewComponent implements OnInit, FormTab {
+  modalRef: BsModalRef;
 
-  constructor() {
+  constructor(private _modalService: BsModalService) {
   }
 
   ngOnInit() {
   }
 
+  saveForm(): void {
+    return;
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this._modalService.show(template);
+  }
 }
